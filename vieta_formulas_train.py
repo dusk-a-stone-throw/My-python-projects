dea01b038a77b6c1261868e83ef28e35a32f2f70
REEPSIW <mihailzomitev@gmail.com>
2022-12-14 13:38:20 +0000

    Better appearence

--- vieta_formulas_train.py
+++ vieta_formulas_train.py
@@ -7,7 +7,7 @@ while True:
     x2 = arr[random.randint(0, len(arr) - 1)]
     while (True):
         print("x²" + "{0:+}x".format(-(x1 + x2)) + "{0:+}".format(x1 * x2))
-        a, b = map(int, input("Enter x1 and x2:").split())
+        a, b = map(int, input("Enter x₁ and x₂:").split())
         if (a == x1 and b == x2) or (a == x2 and b == x1):
             print("You are right.")
             break
