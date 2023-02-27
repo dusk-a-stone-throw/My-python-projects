984c28ed4334847c31e5e06ee69ca2ac099d3515
REEPSIW <mihailzomitev@gmail.com>
2023-02-27 19:30:20 +0000

    EGE, added overlapping

--- Education/EGE/24/NP5643.py
+++ Education/EGE/24/NP5643.py
@@ -2,7 +2,8 @@ import re
 from math import prod
 
 f = open("P5643.txt").readline()
-pattern = re.compile(r"SS12\d{4}77\d{2}9SS")
+# Had to add overlapping matches using ?=
+pattern = re.compile(r"(?=(SS12\d{4}77\d{2}9SS))")
 a = str(max(int(i.replace("SS", "")) for i in re.findall(pattern, f)))
 print(
     sum(int(i) for i in a if int(i) % 2 != 0) +
