5bb63e30b351b9e23e395efd774701bdfdc6cabe
REEPSIW <72338065+REEPSIW@users.noreply.github.com>
2024-01-06 18:28:01 +0300

    EGE

---
+++ Education/EGE/24/NP6147.py
@@ -0,0 +1,17 @@
+f=open("P6147.txt")
+s=f.readline()
+r=set()
+for i in range(len(s)):
+    t=""
+    k=0
+    for j in range(i,len(s)):
+        # no need to check substrings that don't start with "."
+        if k==0 and s[j]!=".":
+            break
+        t+=s[j]
+        if s[j] ==".":
+            k+=1
+        if k==7:
+            r.add(t)
+            break
+print(min([len(i) for i in r]))
