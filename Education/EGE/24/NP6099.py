c5a2068b9f75781e7d95b0e0d22f13ed89b0036b
REEPSIW <72338065+REEPSIW@users.noreply.github.com>
2024-01-07 13:54:31 +0300

    EGE (not optimized)

---
+++ Education/EGE/24/NP6099.py
@@ -0,0 +1,20 @@
+f=open("P6099.txt")
+s=f.readline()
+r=set()
+print("IT'S SLOW AS FK")
+for i in range(len(s)):
+    k=0
+    digits={i for i in "0123456789ABCDEF"}
+    result=[]
+    for j in range(i,len(s)):
+        if k==0 and s[j] not in digits:
+            break
+        result.append(s[j])
+        if s[j] in digits:
+            k+=1
+            digits.remove(s[j])
+        if k==16:
+            r.add("".join(result))
+            break
+print(min([len(i) for i in r]))
+print("At long last...")
